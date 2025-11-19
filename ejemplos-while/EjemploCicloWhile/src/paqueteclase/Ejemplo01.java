f3e8fe097d19bd65f06f0c749f3846492955ff85
alex bravp <fabriciobravo373@gmail.com>
2025-11-19 11:54:23 -0500

    while1

---
+++ ejemplos-while/EjemploCicloWhile/src/paqueteclase/Ejemplo01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paqueteclase;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejemplo01 {
+
+    int contador = 1;
+    while limite = 10;
+    
+    while(contador<=limite){
+
+    System.out.printf ("contador%d\n", contador);
+    contador= contador +1;
+    
+}
